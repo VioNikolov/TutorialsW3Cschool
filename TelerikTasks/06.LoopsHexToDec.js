f41f295165abb1fe2ad4ba0c7ba92aaa5a1f7cea
warax <vionikolov@abv.bg>
2016-12-05 22:33:55 +0200

    got rid off some unnecessery code

--- TelerikTasks/06.LoopsHexToDec.js
+++ TelerikTasks/06.LoopsHexToDec.js
@@ -1,12 +1,12 @@
 function solve(args) {
     var n = args[0],
         hexArr =['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'],
-        slice1 = n.charAt(0), //vzimame stoinostite ot podadeniq n
-        slice0 = n.charAt(1), //
-        hex = 0,
-        numHex1 = hexArr.indexOf(slice1), //obryshtame stoinostite im v chisla
-        numHex0 = hexArr.indexOf(slice0); //
-
+        hex = 0;
+        //slice1 = n.charAt(0), //vzimame stoinostite ot podadeniq n
+        //slice0 = n.charAt(1), //
+        
+        //numHex1 = hexArr.indexOf(slice1), //obryshtame stoinostite im v chisla
+        //numHex0 = hexArr.indexOf(slice0); //
 
 
 
