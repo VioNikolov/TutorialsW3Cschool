616d0ed626f98e785605a5614fccd85823a61f67
warax <vionikolov@abv.bg>
2017-04-02 15:49:02 +0300

    yep

---
+++ TelerikTasks/07.maxSequenceIncreasing.js
@@ -0,0 +1,24 @@
+function solve (args) {
+  
+var n = args.shift(0),
+    count = 1,
+    maxSeq = 1;
+
+for (var i = 0; i < args.length; i++) {
+    if (+args[i] < +args[i+1]){
+        count += 1;
+        if(count > maxSeq){
+            maxSeq = count;
+        }
+    } else {
+        count = 1;
+    }
+    
+}
+
+console.log(maxSeq);
+
+}
+
+
+solve(['8', '7', '3', '2', '3', '4', '2', '2', '4']);
