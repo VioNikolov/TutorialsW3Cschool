0a1c632f7370b645894423ddb463c440a0cdaa4b
warax <vionikolov@abv.bg>
2017-05-17 16:04:45 +0300

    saved

--- SoftUniTasks/12.07.usernames.js
+++ SoftUniTasks/12.07.usernames.js
@@ -1,4 +1,41 @@
-let a = 5;
+function solve(input) {
 
-let b = new String(a);
-console.log(b);
+let obj = {};
+let arr = [];
+
+    for (var i = 0; i < input.length; i++) {
+        if(!(obj.hasOwnProperty(input[i]))) {
+            obj[input[i]] = true;
+        }
+    }
+
+    arr = Object.keys(obj);
+
+    arr.sort(function(x, y) {
+        if (x.length != y.length ) {
+            return x.length - y.length;
+        }
+        else {
+            return x.localeCompare(y);
+        }
+    });
+
+    for (let el of arr) {
+        console.log(el);
+    }
+}
+
+solve([
+'zzzzzz',
+'Kutcher',
+'Ariel',
+'Lilly',
+'Kutcher',
+'xxxxxx',
+'oooooo',
+'Aizen',
+'Bill',
+'Bra'
+]
+
+);
